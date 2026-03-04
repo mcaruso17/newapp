d5ffa8c30d4ef43553173e77c7edb836880e7a06
Matteo Caruso <carusomatteo17@gmail.com>
2026-03-04 10:03:59 +0100

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,7 @@ UFFICI = [str(i) for i in range(1, 14)] + ["CSR I", "CSR II"]
 
 # Dominio email
 EMAIL_DOMAIN = "mef.gov.it"
+
+# Credenziali admin per il primo caricamento
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "qui_metti_una_password_sicura"
